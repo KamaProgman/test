490a1d4fe02760ef529af1bf11de4ca98dacdbab
KamaProgman <latipovkamran1@gmail.com>
2024-03-20 15:19:09 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let temperature = prompt("температура");
+
+// string => number
+
+if (temperature > 30) {
+	alert("жарко");
+} else if (temperature > 15 && temperature <= 30) {
+	alert("тепло");
+} else if (temperature > 0 && temperature <= 15) {
+	alert("прохладно");
+} else {
+	alert("холодно");
+}
+
+// && - оператор И
+// || - оператор ИЛИ
+// != - не строгая отрицательная проверка
+// !== - строгая отрицательная проверка
+
+let time = prompt("время года");
